59dd167709f92c2fc7a3e701a683f78e0109af71
Joe Liu <jiazhou.liu@monash.edu>
2021-02-04 17:37:48 +1100

    updated dashboard

--- Assets/DashBoard.cs
+++ Assets/DashBoard.cs
@@ -4,31 +4,42 @@ using UnityEngine;
 
 public class DashBoard : MonoBehaviour
 {
-    public Transform Camera;
+    public DelaunayController dc;
     public float AdjustedHeight;
     public float ForwardParameter;
     public bool curved;
 
+    private Transform Camera;
+
     // Update is called once per frame
     void Update()
     {
-        transform.position = Camera.TransformPoint(Camera.localPosition + Vector3.forward * ForwardParameter);
-        transform.position = new Vector3(transform.position.x, AdjustedHeight, transform.position.z);
+        if (dc != null && Camera == null)
+            Camera = dc.Human;
+        if (Camera != null) {
+            transform.position = Camera.position + Camera.TransformPoint(Vector3.forward * ForwardParameter);
+            //transform.position = Camera.TransformPoint(Camera.localPosition + Vector3.forward * ForwardParameter);
+            transform.position = new Vector3(transform.position.x, AdjustedHeight, transform.position.z);
 
-        transform.LookAt(Camera);
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + 180, 0);
+            transform.LookAt(Camera);
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + 180, 0);
 
-        if (curved)
-        {
-            foreach (Transform t in transform) {
-                t.LookAt(Camera);
-                t.localEulerAngles = new Vector3(0, t.localEulerAngles.y + 180, 0);
+            if (curved)
+            {
+                foreach (Transform t in transform)
+                {
+                    t.LookAt(Camera);
+                    t.localEulerAngles = new Vector3(0, t.localEulerAngles.y + 180, 0);
+                }
+            }
+            else
+            {
+                foreach (Transform t in transform)
+                {
+                    t.localEulerAngles = new Vector3(0, 0, 0);
+                }
             }
         }
-        else {
-            foreach (Transform t in transform) {
-                t.localEulerAngles = new Vector3(0, 0, 0);
-            }
-        }
+        
     }
 }
