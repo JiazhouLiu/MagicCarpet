b5cfe13cb270eadbec334a4ac284dc5e66e00311
Joe Liu <liujiazhou@gmail.com>
2021-03-29 16:21:38 +1100

    toeTouch Ite1 finished

--- Assets/Script/Controller/FootGestureController.cs
+++ Assets/Script/Controller/FootGestureController.cs
@@ -5,49 +5,56 @@ using UnityEngine;
 enum Gesture { 
     SlideToRight,
     SlideToLeft,
-    RightToeTap,
-    LeftToeTap,
+    ToeRaised,
     None
 }
 
 public class FootGestureController : MonoBehaviour
 {
     public Transform directionIndicator;
-    public Foot leftFoot;
-    public Foot rightFoot;
+    public Transform mainFoot;
+    public Transform mainFootToe;
+    public Transform mainFootHeel;
+    public Transform mainFootToeComponent;
+    public FootCollision FC;
     public Transform interactiveOBJ;
 
-    public int windowFrames = 5;
-    public float scaleFactor = 0.01f;
-    public int GlobalStaticPosCounter = 100;
-    public float GlobalAngleToCancelGes = 20;
-    public float GlobalRaiseFootToCancelGes = 0.18f;
+    public int windowFrames = 5;    // buff frames before detecting sliding
+    public float scaleFactor = 0.01f;     // scale object multiplier
+    public int GlobalStaticPosCounter = 100;    // smooth people stop moving during sliding
+    public float GlobalAngleToCancelGes = 20; // foot with related angle will cancel sliding
+    public float GlobalRaiseFootToCancelGes = 0.18f; // foot over this height will cancel sliding
 
-    private List<Vector3> rightFootLocPositions;
+    // sliding
+    private List<Vector3> mainFootLocPositions;
 
     private Gesture currentGesture;
     private bool passedWindow = false;
     private int staticPosCounter;
 
+    // toe touch
+    private bool toeTap = false;
+    private bool selected = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        rightFootLocPositions = new List<Vector3>();
+        mainFootLocPositions = new List<Vector3>();
         staticPosCounter = GlobalStaticPosCounter;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Left Foot Toe Height: " + leftFoot.Toe.position.y);
-        Debug.Log("Right Foot Toe Height: " + rightFoot.Toe.position.y);
+        //Debug.Log("Right Foot Toe Height: " + mainFootToe.position.y);
+        //Debug.Log("Right Foot Heel Height: " + mainFootHeel.position.y);
 
-        rightFootLocPositions.Add(rightFoot.CurrentTransform.localPosition);
-        if (rightFootLocPositions.Count > windowFrames) {
-            rightFootLocPositions.RemoveAt(0);
+        mainFootLocPositions.Add(mainFoot.localPosition);
+        if (mainFootLocPositions.Count > windowFrames) {
+            mainFootLocPositions.RemoveAt(0);
         }
 
-        if (rightFootLocPositions.Count == windowFrames) {
+        if (mainFootLocPositions.Count == windowFrames) {
             if (!passedWindow)
             {
                 currentGesture = GestureWindowDetector();
@@ -55,7 +62,8 @@ public class FootGestureController : MonoBehaviour
                 if (currentGesture != Gesture.None)
                 {
                     passedWindow = true;
-                    directionIndicator.gameObject.SetActive(true);
+                    if(currentGesture == Gesture.SlideToLeft || currentGesture == Gesture.SlideToRight)
+                        directionIndicator.gameObject.SetActive(true);
                 }
                 else
                 {
@@ -82,6 +90,16 @@ public class FootGestureController : MonoBehaviour
                         ResetIndicator();
                     }
                 }
+
+                if (currentGesture == Gesture.ToeRaised) {
+                    mainFootToeComponent.GetComponent<MeshRenderer>().material.SetColor("_EmissiveColor", Color.green);
+                    if (ToeTapGestureCheck()) {
+                        //Debug.Log("ToeTap");
+                        RunToeTap();
+                        passedWindow = false;
+                        ResetIndicator();
+                    }
+                }
             }
         }
     }
@@ -90,24 +108,56 @@ public class FootGestureController : MonoBehaviour
         foreach (Transform t in directionIndicator)
             t.GetComponent<MeshRenderer>().material.SetColor("_EmissiveColor", Color.black);
         directionIndicator.gameObject.SetActive(false);
+        mainFootToeComponent.GetComponent<MeshRenderer>().material.SetColor("_EmissiveColor", Color.black);
+    }
+
+    private void RunToeTap() {
+        if (!selected)
+        {
+            selected = true;
+            if (FC.TouchedObj != null) {
+                FC.TouchedObj.SetParent(mainFoot);
+                FC.transform.localPosition = Vector3.zero;
+            }
+            
+        }
+        else {
+            selected = false;
+            if (FC.TouchedObj != null)
+                FC.TouchedObj.SetParent(null);
+        }
     }
 
     private Gesture GestureWindowDetector() {
 
+        // sliding
         List<float> angles = new List<float>();
         List<float> distance = new List<float>();
+
+        // toe touch
+        List<float> footToeHeight = new List<float>();
+        List<float> footHeelHeight = new List<float>();
+
+
         List<float> footHeight = new List<float>();
 
         // angles between right direction of right foot and different frames
         for (int i = 0; i < windowFrames - 1; i++) {
-            angles.Add(Vector3.Angle(rightFootLocPositions[i + 1] - rightFootLocPositions[i], rightFoot.CurrentTransform.up));
-            distance.Add(Vector3.Distance(rightFootLocPositions[i + 1], rightFootLocPositions[i]));
-            footHeight.Add(rightFoot.CurrentTransform.position.y);
+            angles.Add(Vector3.Angle(mainFootLocPositions[i + 1] - mainFootLocPositions[i], mainFoot.up));
+            distance.Add(Vector3.Distance(mainFootLocPositions[i + 1], mainFootLocPositions[i]));
+
+            footHeight.Add(mainFoot.position.y);
+
+            footToeHeight.Add(mainFootToe.position.y);
+            footHeelHeight.Add(mainFootHeel.position.y);
         }
 
         bool slideLeft = true;
         bool slideRight = true;
 
+        bool toeRaised = true;
+
+        // sliding
         foreach (float angle in angles.ToArray())
         {
             if (angle == 0 && distance[angles.IndexOf(angle)] == 0) // if stationary
@@ -133,9 +183,46 @@ public class FootGestureController : MonoBehaviour
 
             if (slideRight)
                 return Gesture.SlideToRight;
-        } 
+        }
+
+        // toe touch
+        foreach (float toeHeight in footToeHeight) {
+            if (!(footHeight[footToeHeight.IndexOf(toeHeight)] < 0.15f && mainFootHeel.position.y < 0.02f && toeHeight > 0.1f))
+                toeRaised = false;
+        }
+
+        if (toeRaised)
+            return Gesture.ToeRaised;
+
 
         return Gesture.None;
+    }
+
+    private bool ToeTapGestureCheck() {
+        // toe touch
+        List<float> footToeHeight = new List<float>();
+        List<float> footHeelHeight = new List<float>();
+
+        List<float> footHeight = new List<float>();
+
+        // angles between right direction of right foot and different frames
+        for (int i = 0; i < windowFrames - 1; i++) {
+
+            footHeight.Add(mainFoot.position.y);
+
+            footToeHeight.Add(mainFootToe.position.y);
+            footHeelHeight.Add(mainFootHeel.position.y);
+        }
+
+        // toe touch
+        foreach (float toeHeight in footToeHeight)
+        {
+            if (footHeight[footToeHeight.IndexOf(toeHeight)] < 0.15f && mainFootHeel.position.y < 0.03f && toeHeight <= 0.1f) {
+                toeTap = true;
+                return true;
+            }
+        }
+        return false;
     }
 
     private bool SlideGestureCheck(Gesture gesture)
@@ -147,9 +234,9 @@ public class FootGestureController : MonoBehaviour
         // angles between right direction of right foot and different frames
         for (int i = 0; i < windowFrames - 1; i++)
         {
-            angles.Add(Vector3.Angle(rightFootLocPositions[i + 1] - rightFootLocPositions[i], rightFoot.CurrentTransform.up));
-            distance.Add(Vector3.Distance(rightFootLocPositions[i + 1], rightFootLocPositions[i]));
-            footHeight.Add(rightFoot.CurrentTransform.position.y);
+            angles.Add(Vector3.Angle(mainFootLocPositions[i + 1] - mainFootLocPositions[i], mainFoot.up));
+            distance.Add(Vector3.Distance(mainFootLocPositions[i + 1], mainFootLocPositions[i]));
+            footHeight.Add(mainFoot.position.y);
         }
 
         foreach (float angle in angles.ToArray())
@@ -170,7 +257,6 @@ public class FootGestureController : MonoBehaviour
                         if (resultScale.x < 2)
                             interactiveOBJ.localScale = resultScale;
                     }
-
                 }
 
 
--- Assets/Script/Utilities/FootCollision.cs
+++ Assets/Script/Utilities/FootCollision.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class FootCollision : MonoBehaviour
 {
-
     public DashboardController_PhysicalTouch DC;
+    public Transform TouchedObj;
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.name.Contains("Metropolitan")) {
-            if (name == "LeftFoot")
-                DC.RemoveLeftFootPhysical(other.transform);
-            else
-                DC.RemoveRightFootPhysical(other.transform);
+        if (DC != null)
+        {
+            if (other.name.Contains("Metropolitan"))
+            {
+                if (name == "LeftFoot")
+                    DC.RemoveLeftFootPhysical(other.transform);
+                else
+                    DC.RemoveRightFootPhysical(other.transform);
+            }
         }
-        
+        else {
+            if (other.CompareTag("InteractableObj")) {
+                TouchedObj = null;
+            }
+        } 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name.Contains("Metropolitan"))
+        if (DC != null)
         {
-            if (name == "LeftFoot")
-                DC.RegisterLeftFootPhysical(other.transform);
-            else
-                DC.RegisterRightFootPhysical(other.transform);
+            if (other.name.Contains("Metropolitan"))
+            {
+                if (name == "LeftFoot")
+                    DC.RegisterLeftFootPhysical(other.transform);
+                else
+                    DC.RegisterRightFootPhysical(other.transform);
+            }
+        }
+        else {
+            if (other.CompareTag("InteractableObj"))
+            {
+                
+                TouchedObj = other.transform;
+            }
         }
     }
 }
